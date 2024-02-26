089e0f49464ab3c6127a0df271192e938686442b
praveenkumar <praveen30.rpk@gmail.com>
2024-02-26 19:23:42 +0530

    Update #3

---
+++ index.js
@@ -0,0 +1,12 @@
+// function showContent() {
+//     var content = document.getElementById('content');
+//     content.classList.add('visible');
+//     }
+
+
+// window.addEventListener('load', showContent);
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
